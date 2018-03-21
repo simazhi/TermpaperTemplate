29c5c7dc69f68e351f43c76833d80022dd7b86ac
simazhi <thomas_van_hoey@hotmail.com>
2018-03-21 21:03:04 +0800

    pdf engine take out

--- R/termpaper.R
+++ R/termpaper.R
@@ -1,4 +1,4 @@
-termpaper <- function(toc = TRUE) {
+termpaper <- function(toc = TRUE, toc_depth = 3, highlight = "default", fig_caption = TRUE, ...) {
 
   # get the locations of resource files located within the package
   in_header.tex <- system.file("/Users/Thomas/Desktop/templates/TermpaperTemplate/inst/rmarkdown/templates/termpaper/resources/in_header.tex",
@@ -9,14 +9,18 @@ termpaper <- function(toc = TRUE) {
                                package = "TermpaperTemplate")
 
   # call the base html_document function
-  bookdown::pdf_document2(
-#    toc = toc,
-    latex_engine: xelatex,
+  base <- bookdown::pdf_document2(
+    toc = toc,
+    toc_depth = toc_depth,
+    highlight = highlight,
+#    latex_engine: xelatex,
     keep_tex: keep_tex,
-#    fig_caption: TRUE,
+    fig_caption: fig_caption,
     includes = includes(in_header = in_header.tex,
                         before_body = before_body.tex,
                         after_body = after_body.tex))
+
+  base
 }
 
 
