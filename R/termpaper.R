67798c4f6ca02c2837676f47cbf751b8f877d15a
simazhi <thomas_van_hoey@hotmail.com>
2018-03-21 21:30:34 +0800

    take away includes

--- R/termpaper.R
+++ R/termpaper.R
@@ -18,9 +18,9 @@ termpaper <- function(toc = TRUE, toc_depth = 3, highlight = "default", fig_capt
 #    latex_engine: xelatex,
 #    keep_tex: keep_tex,
     fig_caption: fig_caption,
-    includes = includes(in_header = "in_header.tex",
-                        before_body = "before_body.tex",
-                        after_body = "after_body.tex"),
+#    includes = includes(in_header = "in_header.tex",
+#                        before_body = "before_body.tex",
+#                        after_body = "after_body.tex"),
     ...)
 
   base
