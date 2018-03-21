9196a27036e6d4fec853e96da8d0803736617558
simazhi <thomas_van_hoey@hotmail.com>
2018-03-21 21:16:04 +0800

    change names of objects in includes()

--- R/termpaper.R
+++ R/termpaper.R
@@ -3,11 +3,11 @@
 termpaper <- function(toc = TRUE, toc_depth = 3, highlight = "default", fig_caption = TRUE, ...) {
 
   # get the locations of resource files located within the package
-  in_header.tex <- system.file("/Users/Thomas/Desktop/templates/TermpaperTemplate/inst/rmarkdown/templates/termpaper/resources/in_header.tex",
+  preamble <- system.file("/Users/Thomas/Desktop/templates/TermpaperTemplate/inst/rmarkdown/templates/termpaper/resources/in_header.tex",
                      package = "TermpaperTemplate")
-  before_body.tex <- system.file("/Users/Thomas/Desktop/templates/TermpaperTemplate/inst/rmarkdown/templates/termpaper/resources/before_body.tex",
+  prebody <- system.file("/Users/Thomas/Desktop/templates/TermpaperTemplate/inst/rmarkdown/templates/termpaper/resources/before_body.tex",
                                package = "TermpaperTemplate")
-  after_body.tex <- system.file("/Users/Thomas/Desktop/templates/TermpaperTemplate/inst/rmarkdown/templates/termpaper/resources/after_body.tex",
+  afterbody <- system.file("/Users/Thomas/Desktop/templates/TermpaperTemplate/inst/rmarkdown/templates/termpaper/resources/after_body.tex",
                                package = "TermpaperTemplate")
 
   # call the base html_document function
@@ -18,9 +18,9 @@ termpaper <- function(toc = TRUE, toc_depth = 3, highlight = "default", fig_capt
 #    latex_engine: xelatex,
 #    keep_tex: keep_tex,
     fig_caption: fig_caption,
-    includes = includes(in_header = "in_header.tex",
-                        before_body = "before_body.tex",
-                        after_body = "after_body.tex"),
+    includes = includes(in_header = preamble,
+                        before_body = prebody,
+                        after_body = afterbody),
     ...)
 
   base
